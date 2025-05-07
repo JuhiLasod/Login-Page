9b396833741ee4c033c7671de4b500bc57663f9c
JuhiLasod <juhilasod29@gmail.com>
2025-05-07 10:06:00 +0530

    cors updated

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
 
     const handleCred = async (e) => {
         e.preventDefault();
-        const res = await fetch("http://localhost:8000/api/auth/login", {
+        const res = await fetch("https://login-page-4-qmr9.onrender.com/api/auth/login", {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password })
--- frontend/src/components/sign.jsx
+++ frontend/src/components/sign.jsx
@@ -8,7 +8,7 @@ function Signup() {
     const handleNewAcc = async (e) => {
         e.preventDefault();
         try {
-            const res = await fetch("http://localhost:8000/api/auth/signup", {
+            const res = await fetch("https://login-page-4-qmr9.onrender.com/api/auth/signup", {
                 method: "POST",
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password })
